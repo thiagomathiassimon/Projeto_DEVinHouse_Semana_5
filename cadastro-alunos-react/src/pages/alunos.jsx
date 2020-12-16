561a32ebaa6777e0b7c609e496f5463d1778fef0
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2020-12-16 19:23:58 -0300

    trabalho_no_fomrulario

--- cadastro-alunos-react/src/pages/alunos.jsx
+++ cadastro-alunos-react/src/pages/alunos.jsx
@@ -10,24 +10,61 @@ class Alunos extends Component {
         <Pagina>
           <Section titulo='Cadastro de Alunos'>
             <form>
-              <label htmlFor="nome">Informe o nome do(a) aluno(a):</label>
-              <input type="text" id='nome' placeholder='Nome...' />
-              <label htmlFor="dataNascimento">Informe a data de nascimento do(a) aluno(a):</label>
-              <input type="date" id='dataNascimento' placeholder='Data de nascimento...' />
-              <br />
-              <label htmlFor="nomeResponsavel">Informe o nome do(a) responsável:</label>
-              <input type="text" id='nomeResponsavel' placeholder='Nome...' />
-              <label htmlFor="telefone">Informe o telefone do(a) responsável:</label>
-              <input type="number" id='telefone' placeholder='Telefone...' />
-              <br />
-              <label htmlFor="emergencia">Em caso de emergência avisar:</label>
-              <select name="emergencia" id="emergencia">
-                <option value="opcao0">Selecione uma opção</option>
-                <option value="opcao1">Pais</option>
-                <option value="opcao2">Tios</option>
-                <option value="opcao3">Avós</option>
-                <option value="opcao4">Padrinhos/Madrinhas</option>
-              </select>
+              <table>
+                <tr>
+                  <td><label htmlFor="nome">Informe o nome do(a) aluno(a):</label></td>
+                  <td><input type="text" id='nome' placeholder='Nome...' /></td>
+                </tr>
+                <tr>
+                  <td><label htmlFor="dataNascimento">Informe a data de nascimento do(a) aluno(a):</label></td>
+                  <td><input type="date" id='dataNascimento' placeholder='Data de nascimento...' /></td>
+                </tr>
+                <tr>
+                  <td>
+                    <label htmlFor="nomeResponsavel">Informe o nome do(a) responsável:</label>
+                  </td>
+                  <td>
+                    <input type="text" id='nomeResponsavel' placeholder='Nome...' />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <label htmlFor="telefone">Informe o telefone do(a) responsável:</label>
+                  </td>
+                  <td>
+                    <input type="number" id='telefone' placeholder='Telefone...' />
+                  </td>
+                </tr>
+                <tr>
+                  <td>       <label htmlFor="emergencia">Em caso de emergência avisar:</label></td>
+                  <td>
+                    <select name="emergencia" id="emergencia">
+                      <option value="opcao0">Selecione uma opção</option>
+                      <option value="opcao1">Pais</option>
+                      <option value="opcao2">Tios</option>
+                      <option value="opcao3">Avós</option>
+                      <option value="opcao4">Padrinhos/Madrinhas</option>
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <label htmlFor="telefoneEmergencia">Informe o telefone de emergência:</label>
+                  </td>
+                  <td>
+                    <input type="number" id='telefoneEmergencia' placeholder='Telefone...' />
+                  </td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>
+                    <button id='cancelar'>Cancelar</button>
+                    <button id='cadastrar'>Cadastrar</button></td>
+                </tr>
+              </table>
+
+
+
             </form>
           </Section>
           <Section titulo='Listagem de Alunos'>
